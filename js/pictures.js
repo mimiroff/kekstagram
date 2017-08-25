12065286316b78ace2f0218e693426ae68e6fcc4
Ilia Barashkov <ibarashkov@list.ru>
2017-08-25 17:04:57 +0300

    task 4.1

--- js/pictures.js
+++ js/pictures.js
@@ -34,7 +34,7 @@ var generateElements = function (photo) {
   var photoElement = document.querySelector('#picture-template').content.cloneNode(true);
   photoElement.querySelector('img').src = photo.url;
   photoElement.querySelector('.picture-likes').textContent = photo.likes;
-  photoElement.querySelector('.picture-comments').textContent = photo.comments;
+  photoElement.querySelector('.picture-comments').textContent = photo.comments.length.toString();
 
   return photoElement;
 };
@@ -46,12 +46,57 @@ var showPhotoElements = function (photos) {
     fragment.appendChild(generateElements(photos[i]));
   }
   pictures.appendChild(fragment);
-
-  var gallery = document.querySelector('.gallery-overlay');
-  gallery.classList.remove('hidden');
-  gallery.querySelector('.gallery-overlay-image').src = photos[0].url;
-  gallery.querySelector('.likes-count').textContent = photos[0].likes;
-  gallery.querySelector('.comments-count').textContent = photos[0].comments;
 };
 
 showPhotoElements(generatePhotos(25));
+
+var gallery = document.querySelector('.gallery-overlay');
+var pictureList = document.querySelectorAll('.picture');
+var galleryClose = gallery.querySelector('.gallery-overlay-close');
+var keyCodes = {
+  ENTER: 13,
+  ESC: 27
+};
+
+var closePopup = function () {
+  gallery.classList.add('hidden');
+};
+
+var openPopup = function (evt) {
+  var image = evt.currentTarget.querySelector('img').src;
+  var likes = evt.currentTarget.querySelector('.picture-likes').textContent;
+  var comments = evt.currentTarget.querySelector('.picture-comments').textContent;
+
+  gallery.classList.remove('hidden');
+  gallery.querySelector('.gallery-overlay-image').src = image;
+  gallery.querySelector('.likes-count').textContent = likes;
+  gallery.querySelector('.comments-count').textContent = comments;
+
+  galleryClose.addEventListener('click', function () {
+    closePopup();
+  });
+  galleryClose.addEventListener('keydown', function (event) {
+    if (event.keyCode === keyCodes.ENTER) {
+      closePopup();
+    }
+  });
+  document.addEventListener('keydown', function (event) {
+    if (event.keyCode === keyCodes.ESC) {
+      closePopup();
+    }
+  });
+};
+
+for (var i = 0; i < pictureList.length; i++) {
+  pictureList[i].addEventListener('click', function (evt) {
+    evt.preventDefault();
+    openPopup(evt);
+  });
+
+  pictureList[i].addEventListener('keydown', function (evt) {
+    evt.preventDefault();
+    if (evt.keyCode === keyCodes.ENTER) {
+      openPopup(evt);
+    }
+  });
+}
